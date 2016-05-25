ab2e15fde54f659000c9637634e08c6a3bae9083
Matteo <matteo1.frosi@mail.polimi.it>
2016-05-25 12:50:19 +0200

    Wealth Path Test.

---
+++ test/core/gamelogic/TownBonusFactoryTest.java
@@ -0,0 +1,16 @@
+package core.gamelogic;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Matteo on 25/05/16.
+ */
+public class TownBonusFactoryTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
--- test/core/gamemodel/WealthPathTest.java
+++ test/core/gamemodel/WealthPathTest.java
@@ -1,4 +1,4 @@
-package test;
+package core.gamemodel;
 
 import core.Player;
 import core.gamemodel.VictoryPath;
