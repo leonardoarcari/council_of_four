4ee8f71d416769698c07c2500ddca95395d5dbb5
Matteo <matteo1.frosi@mail.polimi.it>
2016-06-01 11:15:44 +0200

    All the cities.

--- src/client/GUI.java
+++ src/client/GUI.java
@@ -74,14 +74,28 @@ public class GUI extends Application {
         // Town IVs
         Image aImage = new Image(new FileInputStream("src/client/a.png"));
         Image bImage = new Image(new FileInputStream("src/client/b.png"));
+        Image cImage = new Image(new FileInputStream("src/client/c.png"));
+        Image dImage = new Image(new FileInputStream("src/client/d.png"));
+        Image eImage = new Image(new FileInputStream("src/client/e.png"));
+        Image fImage = new Image(new FileInputStream("src/client/f.png"));
         Image gImage = new Image(new FileInputStream("src/client/g.png"));
         Image hImage = new Image(new FileInputStream("src/client/h.png"));
         Image iImage = new Image(new FileInputStream("src/client/i.png"));
+        Image mImage = new Image(new FileInputStream("src/client/m.png"));
+        Image nImage = new Image(new FileInputStream("src/client/n.png"));
+        Image oImage = new Image(new FileInputStream("src/client/o.png"));
         boardObjects.add(new ObjectImageView(aImage, 0.07257407407407407, 0.059109289617486336, 0.10459153122197));
-        boardObjects.add(new ObjectImageView(bImage, 0.061342592592592594, 0.24180327868852458, 0.113425925925926));
+        boardObjects.add(new ObjectImageView(bImage, 0.061042592592592594, 0.24180327868852458, 0.114425925925926));
+        boardObjects.add(new ObjectImageView(cImage, 0.2155925925925926, 0.11958469945355191, 0.114583333333333));
+        boardObjects.add(new ObjectImageView(dImage, 0.2084852504710498, 0.2786885245901639, 0.105321313772738));
+        boardObjects.add(new ObjectImageView(eImage, 0.11026557621929187, 0.40846994535519127, 0.102691805475035));
+        boardObjects.add(new ObjectImageView(fImage, 0.3811597344042335, 0.0868013698630137, 0.104449317367015));
         boardObjects.add(new ObjectImageView(gImage, 0.3948916963480114, 0.2467627118644068, 0.10285385614803205));
         boardObjects.add(new ObjectImageView(hImage, 0.40973005099866394, 0.3864406779661017, 0.09912460333496036));
         boardObjects.add(new ObjectImageView(iImage, 0.5466258390659746, 0.08813559322033898, 0.0967313203267906));
+        boardObjects.add(new ObjectImageView(mImage, 0.6729745030117486, 0.416462482946794, 0.120203003974608));
+        boardObjects.add(new ObjectImageView(nImage, 0.82539565232543, 0.16800354706684858, 0.113268215283765));
+        boardObjects.add(new ObjectImageView(oImage, 0.829096739437645, 0.3542896174863388, 0.106006559623886));
         boardObjects.forEach(objectImageView -> setObjectGlow(objectImageView, glow));
 
         // Add Nodes to anchorPane
